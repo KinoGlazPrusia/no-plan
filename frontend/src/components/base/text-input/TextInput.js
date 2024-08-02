409aa7920f827fe8270b583728d7443e804166e5
Josep <jmartinbolet@gmail.com>
2024-08-02 10:55:28 +0200

    Refactoring

--- frontend/src/components/base/text-input/TextInput.js
+++ frontend/src/components/base/text-input/TextInput.js
@@ -1,28 +1,32 @@
-import { PlainComponent, PlainState } from '../../../../node_modules/plain-reactive/src/index.js'
+import {
+  PlainComponent,
+  PlainState
+} from '../../../../node_modules/plain-reactive/src/index.js'
 import { BASE_COMPONENTS_PATH } from '../../../config/env.config.js'
 
 /* COMPONENTS */
-/* eslint-disable */
 import FormFeedback from '../form-feedback/FormFeedback.js'
-/* eslint-enable */
 
 /* SERVICES */
 import * as validators from '../../../utils/validators.js'
 
 class TextInput extends PlainComponent {
-  constructor () {
+  constructor() {
     super('p-text-input', `${BASE_COMPONENTS_PATH}text-input/TextInput.css`)
 
     this.validator = validators[this.getAttribute('validator')]
-    this.validity = new PlainState({
-      isValid: true,
-      messages: []
-    }, this)
+    this.validity = new PlainState(
+      {
+        isValid: true,
+        messages: []
+      },
+      this
+    )
 
     this.inputValue = new PlainState('', this)
   }
 
-  template () {
+  template() {
     return `
             <label class="label">${this.getAttribute('label')}</label>
 
@@ -38,7 +42,7 @@ class TextInput extends PlainComponent {
         `
   }
 
-  listeners () {
+  listeners() {
     this.$('.input').oninput = (e) => {
       // Actualización del input value
       this.updateValue()
@@ -48,34 +52,37 @@ class TextInput extends PlainComponent {
     }
   }
 
-  updateValue () {
+  updateValue() {
     this.inputValue.setState(this.$('.input').value, false)
   }
 
-  validate () {
+  validate() {
     const value = this.$('.input').value
 
     let isValid = null
     const validityMessage = this.validator(value)
 
-    validityMessage.length > 0
-      ? isValid = false
-      : isValid = true
+    validityMessage.length > 0 ? (isValid = false) : (isValid = true)
 
     !isValid
-      ? !this.wrapper.classList.contains('is-invalid') && this.wrapper.classList.add('is-invalid')
-      : this.wrapper.classList.contains('is-invalid') && this.wrapper.classList.remove('is-invalid')
+      ? !this.wrapper.classList.contains('is-invalid') &&
+        this.wrapper.classList.add('is-invalid')
+      : this.wrapper.classList.contains('is-invalid') &&
+        this.wrapper.classList.remove('is-invalid')
 
-    this.validity.setState({
-      isValid,
-      messages: validityMessage
-    }, false)
+    this.validity.setState(
+      {
+        isValid,
+        messages: validityMessage
+      },
+      false
+    )
 
     // Re-renderizamos el componente de feedback para mostrar errores o mensajes
     this.updateFeedback()
   }
 
-  updateFeedback () {
+  updateFeedback() {
     // Re-renderizamos el componente de feedback para mostrar errores o mensajes
     this.$('.feedback').errors.setState(this.validity.getState().messages)
   }
