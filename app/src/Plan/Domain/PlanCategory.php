29db386042c8c5a59334ea05f7d743d91942c146
Josep <jmartinbolet@gmail.com>
2024-08-09 13:30:51 +0200

    Implemented fetchAllCategories()

---
+++ app/src/Plan/Domain/PlanCategory.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Plan\Domain;
+
+use App\Core\Domain\CoreEntity;
+
+class PlanCategory extends CoreEntity {
+    public readonly int | null $id;
+    public readonly string | null $name;
+    public readonly string | null $description;
+
+    public function __construct(Object | null $data = null) {
+        parent::__construct(
+            'plan_category',
+            [
+                'id',
+                'name',
+                'description',
+            ]
+        );
+
+        $this->id = isset($data->id) ? $data->id : null;
+        $this->name = isset($data->name) ? $data->name : null;
+        $this->description = isset($data->description) ? $data->description : null;
+    }
+
+    /* SETTERS */
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+
+    public function setDescription(string $description): void {
+        $this->description = $description;
+    }
+}
