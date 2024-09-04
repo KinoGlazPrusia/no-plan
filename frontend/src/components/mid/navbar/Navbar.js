f0fa0b732517c5ff064791aa6994c0481a6dbd9a
Foap2023 <Foap2023@BI101-02.epsevg.upc.es>
2024-09-04 13:18:44 +0200

    Updated

--- frontend/src/components/mid/navbar/Navbar.js
+++ frontend/src/components/mid/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { PlainComponent, PlainContext } from '../../../../node_modules/plain-reactive/src/index.js'
+import { PlainComponent, PlainContext, PlainState } from '../../../../node_modules/plain-reactive/src/index.js'
 import { MID_COMPONENTS_PATH } from '../../../config/env.config.js'
 
 /* COMPONENTS */
@@ -12,6 +12,8 @@ class Navbar extends PlainComponent {
     super('p-navbar', `${MID_COMPONENTS_PATH}navbar/Navbar.css`)
 
     this.navigationContext = new PlainContext('navigation', this)
+
+    this.notifications = new PlainState(0, this)
   }
 
    async connectedCallback () {
@@ -21,18 +23,33 @@ class Navbar extends PlainComponent {
    
   async loadNotifications () {
     const unreadNotifications = await apiNotification.getUnreadNotifications()
-    console.log(unreadNotifications)
+    this.notifications.setState(unreadNotifications.data.length)
   }
 
   // [ ] Implementar deshabilitación de botones dependiendo del estado de autenticación del usuario
   template () {
+    const notificationChip = () => {
+      const notificationNum = this.notifications.getState()
+      return notificationNum > 0 
+        ? `<span class="notification-chip pop">${notificationNum}</span>`
+        : ''
+    }
     return `
             <p-navbar-button icon="person" path="user/profile" ${this.navigationContext.getData('currentPage') === 'user/profile' ? 'selected="true' : ''}"></p-navbar-button>
             <p-navbar-button icon="event" path="user/plans" ${this.navigationContext.getData('currentPage') === 'user/plans' ? 'selected="true"' : ''}"></p-navbar-button>
             <p-navbar-button icon="add" path="plan/create" ${this.navigationContext.getData('currentPage') === 'plan/create' ? 'selected="true' : ''}"></p-navbar-button>
             <p-navbar-button icon="roofing" path="planner" ${this.navigationContext.getData('currentPage') === 'planner' ? 'selected="true' : ''}"></p-navbar-button>
             <p-navbar-button icon="search" path="finder" ${this.navigationContext.getData('currentPage') === 'finder' ? 'selected="true' : ''}"></p-navbar-button>
+            ${notificationChip()}
         `
+  }
+
+  listeners () {
+    if (this.$('.notification-chip')) {
+      this.$('.notification-chip').onanimationend = () => {
+        this.$('.notification-chip').classList.remove('pop')
+      }
+    }
   }
 }
 
